a758bc6c365da0f03800398b757824e06daa6e77
Agabala.G <pakka05@gmail.com>
2025-05-12 21:44:33 +0300

    12u

--- dz.py
+++ dz.py
@@ -1,39 +1,108 @@
 import json
 
-data = {
-    "1441751072": {
-        "name": "ebeqbqa",
-        "tel": "7979251508"
-    },
-    "6384137383": {
-        "name": "daeefed",
-        "tel": "6253294228"
-    },
-    "8186377272": {
-        "name": "bdbqeab",
-        "tel": "9543402193"
-    },
-    "7194236131": {
-        "name": "gafdbee",
-        "tel": "6265065413"
-    },
-    "3313812937": {
-        "name": "gbfddgg",
-        "tel": "5668240979"
-    }
-}
 
-print(json.dumps(data, indent=4))
+class CountryCapital:
+    @staticmethod
+    def load(file_name):
+        try:
+            data = json.load(open(file_name, encoding="utf-8"))
+        except FileNotFoundError:
+            data = {}
+        finally:
+            return data
 
-key = "1441751072"
-if key in data:
-    name = data[key]["name"]
-    print(f"Имя для ключа {key}: {name}")
-else:
-    print(f"Ключ {key} не найден.")
+    @staticmethod
+    def add_country(file_name):
+        new_country = input("Введите название страны: ").lower()
+        new_capital = input("Введите название столицы: ").lower()
 
-for key, value in data.items():
-    name = value["name"]
-    tel = value["tel"]
-    print(f"Ключ: {key}, Имя: {name}, Телефон: {tel}")
+        # try:
+        #     data = json.load(open(file_name, encoding="utf-8"))
+        # except FileNotFoundError:
+        #     data = {}
+        data = CountryCapital.load(file_name)
 
+        data[new_country] = new_capital
+
+        with open(file_name, "w", encoding="utf-8") as f:
+            json.dump(data, f, indent=2, ensure_ascii=False)
+
+    @staticmethod
+    def load_from_file(file_name):
+        with open(file_name, encoding="utf-8") as f:
+            print({k.title(): v.title() for k, v in json.load(f).items()})
+
+    @staticmethod
+    def delete_country(file_name):
+        del_country = input("Введите название страны: ").lower()
+
+        # try:
+        #     data = json.load(open(file_name, encoding="utf-8"))
+        # except FileNotFoundError:
+        #     data = {}
+        data = CountryCapital.load(file_name)
+
+        if del_country in data:
+            del data[del_country]
+
+            with open(file_name, "w", encoding="utf-8") as f:
+                json.dump(data, f, indent=2, ensure_ascii=False)
+        else:
+            print("Такой страны в базе нет")
+
+    @staticmethod
+    def search_data(file_name):
+        country = input("Введите название страны: ").lower()
+
+        # try:
+        #     data = json.load(open(file_name, encoding="utf-8"))
+        # except FileNotFoundError:
+        #     data = {}
+        data = CountryCapital.load(file_name)
+
+        if country in data:
+            print(f"Страны {country.title()} столица {data[country].title()} есть в словаре")
+        else:
+            print(f"Страны {country.title()} нет в словаре")
+
+    @staticmethod
+    def edit_data(file_name):
+        country = input("Введите страну для корректировки: ").lower()
+        new_capital = input("Введите новое название столицы: ").lower()
+
+        # try:
+        #     data = json.load(open(file_name, encoding="utf-8"))
+        # except FileNotFoundError:
+        #     data = {}
+        data = CountryCapital.load(file_name)
+
+        if country in data:
+            data[country] = new_capital
+
+            with open(file_name, "w", encoding="utf-8") as f:
+                json.dump(data, f, indent=2, ensure_ascii=False)
+        else:
+            print("Такой страны в базе нет")
+
+
+file = "list_capital.json"
+while True:
+    index = input("Выбор действия:\n1 - добавление данных\n2 - удаление данных\n3 - поиск данных\n"
+                  "4 - редактирование данных\n5 - просмотр данных\n6 - завершение работы\n"
+                  "Ввод: ")
+    if index == "1":
+        CountryCapital.add_country(file)
+    elif index == "2":
+        CountryCapital.delete_country(file)
+    elif index == "3":
+        CountryCapital.search_data(file)
+    elif index == "4":
+        CountryCapital.edit_data(file)
+    elif index == "5":
+        CountryCapital.load_from_file(file)
+    elif index == "6":
+        break
+    else:
+        print("Введен некорректный номер")
+
+    print("*" * 50)
--- dzzz1.py
+++ dzzz1.py
@@ -1,179 +1,105 @@
-# import datetime
-#
-# class MyTime:
-#     """
-#     Класс для представления времени в формате ЧЧ:ММ:СС.
-#     """
-#     def __init__(self, hours, minutes, seconds):
-#         """
-#         Инициализация объекта MyTime.
-#         """
-#         self.hours = hours
-#         self.minutes = minutes
-#         self.seconds = seconds
-#
-#     def __str__(self):
-#         """
-#         Строковое представление времени.
-#         """
-#         return f"{self.hours:02}:{self.minutes:02}:{self.seconds:02}"
-#
-#     def to_seconds(self):
-#         """
-#         Преобразование времени в секунды.
-#         """
-#         return self.hours * 3600 + self.minutes * 60 + self.seconds
-#
-#     @classmethod
-#     def from_seconds(cls, total_seconds):
-#         """
-#         Создание объекта MyTime из общего количества секунд.
-#         """
-#         hours = total_seconds // 3600
-#         total_seconds %= 3600
-#         minutes = total_seconds // 60
-#         seconds = total_seconds % 60
-#         return cls(hours, minutes, seconds)
-#
-#     def __gt__(self, other):
-#         """Перегрузка оператора >."""
-#         return self.to_seconds() > other.to_seconds()
-#
-#     def __ge__(self, other):
-#         """Перегрузка оператора >=."""
-#         return self.to_seconds() >= other.to_seconds()
-#
-#     def __lt__(self, other):
-#         """Перегрузка оператора <."""
-#         return self.to_seconds() < other.to_seconds()
-#
-#     def __le__(self, other):
-#         """Перегрузка оператора <=."""
-#         return self.to_seconds() <= other.to_seconds()
-#
-#     def __eq__(self, other):
-#         """Перегрузка оператора ==."""
-#         return self.to_seconds() == other.to_seconds()
-#
-#     def __ne__(self, other):
-#         """Перегрузка оператора !=."""
-#         return self.to_seconds() != other.to_seconds()
-#
-#
-# # Пример использования
-# c1 = MyTime(0, 10, 0)
-# c3 = MyTime(0, 15, 0)
-#
-# print(f"c3 > c1 {c3 > c1}")   # True
-# print(f"c3 >= c1 {c3 >= c1}")  # True
-# print(f"c3 < c1 {c3 < c1}")   # False
-# print(f"c3 <= c1 {c3 <= c1}")  # False
+import json
+
+def load_data(filename="countries.json"):
+    """Loads data from a JSON file.  If the file doesn't exist, returns an empty dictionary."""
+    try:
+        with open(filename, 'r', encoding='utf-8') as f:
+            data = json.load(f)
+        return data
+    except FileNotFoundError:
+        return {}
+    except json.JSONDecodeError:  #Handle possible errors if the file has been tampered with.
+        print("Error: The data file is corrupted.  Starting with an empty database.")
+        return {}
 
 
-import datetime
+def save_data(data, filename="countries.json"):
+    """Saves data to a JSON file."""
+    with open(filename, 'w', encoding='utf-8') as f:
+        json.dump(data, f, indent=4, ensure_ascii=False)  #Added indent for readability
 
 
-class MyTime:
-    """
-    Класс для представления времени в формате ЧЧ:ММ:СС.
-    """
+def add_data(data):
+    """Adds a new country and capital to the dictionary."""
+    country = input("Введите название страны (с заглавной буквы): ")
+    capital = input("Введите название столицы страны (с заглавной буквы): ")
+    data[country] = capital
+    print("Файл сохранен")
+    return data
 
-    def __init__(self, hours, minutes, seconds):
+def delete_data(data):
+    """Deletes a country from the dictionary."""
+    country = input("Введите название страны для удаления (с заглавной буквы): ")
+    if country in data:
+        del data[country]
+        print("Данные удалены.")
+    else:
+        print("Страна не найдена.")
+    return data
 
-        self.hours = hours
-        self.minutes = minutes
-        self.seconds = seconds
 
-    def __str__(self):
+def search_data(data):
+    """Searches for a country and prints its capital."""
+    country = input("Введите название страны для поиска (с заглавной буквы): ")
+    if country in data:
+        print(f"Столица страны {country}: {data[country]}")
+    else:
+        print("Страна не найдена.")
 
-        return f"{self.hours:02}:{self.minutes:02}:{self.seconds:02}"
 
-    def to_seconds(self):
-        return self.hours * 3600 + self.minutes * 60 + self.seconds
+def edit_data(data):
+    """Edits the capital of an existing country."""
+    country = input("Введите название страны для редактирования (с заглавной буквы): ")
+    if country in data:
+        new_capital = input(f"Введите новое название столицы для страны {country} (с заглавной буквы): ")
+        data[country] = new_capital
+        print("Данные обновлены.")
+    else:
+        print("Страна не найдена.")
+    return data
 
-    @classmethod
-    def from_seconds(cls, total_seconds):
-        hours = total_seconds // 3600
-        total_seconds %= 3600
-        minutes = total_seconds // 60
-        seconds = total_seconds % 60
-        return cls(hours, minutes, seconds)
 
-    def __sub__(self, other):
-        seconds1 = self.to_seconds()
-        seconds2 = other.to_seconds()
-        result_seconds = seconds1 - seconds2
-        if result_seconds < 0:
-            result_seconds = 0
-        return MyTime.from_seconds(result_seconds)
+def view_data(data):
+    """Prints the entire dictionary of countries and capitals."""
+    print(data)
 
-    def __mul__(self, other):
-        seconds = self.to_seconds()
-        result_seconds = seconds * other
-        return MyTime.from_seconds(result_seconds)
 
-    def __floordiv__(self, other):
-        seconds = self.to_seconds()
-        result_seconds = seconds // other
-        return MyTime.from_seconds(result_seconds)
+def main():
+    """Main function to run the program."""
+    data = load_data()
 
-    def __mod__(self, other):
-        seconds = self.to_seconds()
-        result_seconds = seconds % other
-        return MyTime.from_seconds(result_seconds)
+    while True:
+        print("*" * 20)
+        print("Выбор действия:")
+        print("1 - добавление данных")
+        print("2 - удаление данных")
+        print("3 - поиск данных")
+        print("4 - редактирование данных")
+        print("5 - просмотр данных")
+        print("6 - завершение работы")
+        print("*" * 20)
 
-    def __isub__(self, other):
-        result = self - other
-        self.hours = result.hours
-        self.minutes = result.minutes
-        self.seconds = result.seconds
-        return self
+        choice = input("Ввод: ")
 
-    def __imul__(self, other):
-        result = self * other
-        self.hours = result.hours
-        self.minutes = result.minutes
-        self.seconds = result.seconds
-        return self
+        if choice == '1':
+            data = add_data(data)
+            save_data(data)
+        elif choice == '2':
+            data = delete_data(data)
+            save_data(data)
+        elif choice == '3':
+            search_data(data)
+        elif choice == '4':
+            data = edit_data(data)
+            save_data(data)
+        elif choice == '5':
+            view_data(data)
+        elif choice == '6':
+            print("Завершение работы.")
+            break
+        else:
+            print("Неверный ввод. Пожалуйста, выберите действие от 1 до 6.")
 
-    def __ifloordiv__(self, other):
-        result = self // other
-        self.hours = result.hours
-        self.minutes = result.minutes
-        self.seconds = result.seconds
-        return self
 
-    def __imod__(self, other):
-        result = self % other
-        self.hours = result.hours
-        self.minutes = result.minutes
-        self.seconds = result.seconds
-        return self
-
-c1 = MyTime(0, 10, 0)
-c2 = MyTime(0, 3, 20)
-
-print(f"c1: {c1}")
-
-print(f"c1 - c2: {c1 - c2}")
-print(f"c1 * c2: {c1 * 3}")
-print(f"c1 // c2: {c1 // 3}")
-print(f"c1 % c2: {c1 % 3}")
-
-c3 = MyTime(0, 10, 0)
-c4 = MyTime(0, 3, 20)
-
-c3 -= c4
-print(f"c1 -= c2: {c3}")
-
-c3 = MyTime(0, 10, 0)
-c3 *= 3
-print(f"c1 *= c2: {c3}")
-
-c3 = MyTime(0, 10, 0)
-c3 //= 3
-print(f"c1 //= c2: {c3}")
-
-c3 = MyTime(0, 10, 0)
-c3 %= 3
-print(f"c1 %= c2: {c3}")
+if __name__ == "__main__":
+    main()
